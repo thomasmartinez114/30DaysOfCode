ebf9f8365a06022bf46aab3a739bcebf066aee83
Thomas Martinez <tmartinez114@gmail.com>
2024-05-11 15:34:09 -0400

    updated Math.floor to Math.round and issue resolvd

--- day002-operators.js
+++ day002-operators.js
@@ -14,17 +14,17 @@
  *  3. INTEGER tax_percent
  */
 
-let meal_cost = 12.0
-let tip_percent = 20
-let tax_percent = 8
+let meal_cost = 10.25
+let tip_percent = 17
+let tax_percent = 5
 
 function solve(meal_cost, tip_percent, tax_percent) {
   // Write your code here
   let tip = meal_cost * (tip_percent / 100)
-  // console.log(tip)
+  // console.log(`Tip ${tip}`)
   let tax = meal_cost * (tax_percent / 100)
   // console.log(tax)
-  let total_cost = Math.floor(meal_cost + tip + tax)
+  let total_cost = Math.round(meal_cost + tip + tax)
   console.log(total_cost)
 }
 
